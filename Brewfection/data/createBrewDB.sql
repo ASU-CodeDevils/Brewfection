8e033b060a45b356332e304e79a478604e2ac2c3
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-12-05 10:21:00 -0600

    Most of the way through converting the sql files to make a PostgreSQL db.

--- Brewfection/data/createBrewDB.sql
+++ Brewfection/data/createBrewDB.sql
@@ -1,57 +1,65 @@
-# noinspection SqlNoDataSourceInspectionForFile
+-- noinspection SqlNoDataSourceInspectionForFile
 
-# This should only be run once, to create all the tables for the database.
+-- This should only be run once, to create all the tables for the database.
 
-DROP DATABASE IF EXISTS BrewSimDB;
-CREATE DATABASE BrewSimDB;
-USE BrewSimDB;
+-- DROP DATABASE IF EXISTS BrewSimDB;
+-- CREATE DATABASE BrewSimDB;
+-- USE BrewSimDB;
+
+/*  In the command line, run the following to initialize the database.
+    createdb brewdb
+    psql -U kchoadley brewdb -f createBrewDB.sql
+    */
 
 CREATE TABLE grain(
-  #Below is generic ingredient attributes
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  --Below is generic ingredient attributes
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   description VARCHAR(255),
-  #Below is unique attributes
+  --Below is unique attributes
   region VARCHAR(30),
   potential_extract INT NOT NULL,
   lovibonds INT NOT NULL
 );
 
+CREATE TYPE purpose as ENUM('bitter','aroma','dual');
 CREATE TABLE hops(
-  #Below is generic ingredient attributes
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  --Below is generic ingredient attributes
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   description VARCHAR(255),
-  #Below is unique attributes
+  --Below is unique attributes
   region VARCHAR(30),
-  alpha_acid DOUBLE(3,1) NOT NULL, #3 = max digits, 1 = precision right of decimal
-  purpose ENUM('bitter','aroma','dual'),
+  alpha_acid DECIMAL(3,1) NOT NULL, --3 = max digits, 1 = precision right of decimal
+  purpose purpose,
   aroma VARCHAR(120)
 );
 
+
+CREATE TYPE yeast_type as ENUM('ale','lager','other');
 CREATE TABLE yeast(
-  #Below is generic ingredient attributes
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  --Below is generic ingredient attributes
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   description VARCHAR(255),
-  #Below is unique attributes
+  --Below is unique attributes
   region VARCHAR(30),
   brand VARCHAR(30),
-  apparent_attenuation DOUBLE(3,2) NOT NULL, #percentage, usually 65%-80%
-  yeast_type ENUM('ale','lager','other') NOT NULL
+  apparent_attenuation DECIMAL(3,2) NOT NULL, --percentage, usually 65%-80%
+  yeast_type yeast_type NOT NULL
 );
 
 CREATE TABLE additive(
-  #Below is generic ingredient attributes
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  --Below is generic ingredient attributes
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   description VARCHAR(255),
-  #Below is unique attributes
+  --Below is unique attributes
   use_case VARCHAR(30)
 );
 
 CREATE TABLE beer_recipe(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   boil_time INT NOT NULL,
   description VARCHAR(255),
@@ -59,34 +67,34 @@ CREATE TABLE beer_recipe(
 );
 
 CREATE TABLE beer_style(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   description VARCHAR(255),
   origin VARCHAR(30),
-  min_bitterness DOUBLE(5,1) NOT NULL, # could be over 100
-  max_bitterness DOUBLE(5,1) NOT NULL, # could be over 100, highest found was 2500
-  min_color DOUBLE(3,1) NOT NULL, # scale of 1-40
-  max_color DOUBLE(3,1) NOT NULL, # scale of 1-40
-  min_ABV DOUBLE(3,1) NOT NULL, # percent, usually from 3%-12%
-  max_ABV DOUBLE(3,1) NOT NULL # percent, usually from 3%-12%
+  min_bitterness DECIMAL(5,1) NOT NULL, -- could be over 100
+  max_bitterness DECIMAL(5,1) NOT NULL, -- could be over 100, highest found was 2500
+  min_color DECIMAL(3,1) NOT NULL, -- scale of 1-40
+  max_color DECIMAL(3,1) NOT NULL, -- scale of 1-40
+  min_ABV DECIMAL(3,1) NOT NULL, -- percent, usually from 3%-12%
+  max_ABV DECIMAL(3,1) NOT NULL -- percent, usually from 3%-12%
 );
 
 CREATE TABLE equipment(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  batch_size DOUBLE(7,1), # In gallons, allows up to 3000 barrel system.
-  extract_efficeny DOUBLE(3,1) # percent, usually 60%-80%
+  id SERIAL NOT NULL PRIMARY KEY,
+  batch_size DECIMAL(7,1), -- In gallons, allows up to 3000 barrel system.
+  extract_efficeny DECIMAL(3,1) -- percent, usually 60%-80%
 );
 
 CREATE TABLE style_of_recipe(
   recipe_id INT NOT NULL,
   style_id INT NOT NULL,
-  PRIMARY KEY (recipe_id), # there can only be one beer style per recipe
+  PRIMARY KEY (recipe_id), -- there can only be one beer style per recipe
   FOREIGN KEY (style_id)
   REFERENCES beer_style(id)
-    ON UPDATE CASCADE ON DELETE CASCADE, # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE, -- need to determine if this is correct...
   FOREIGN KEY (recipe_id)
   REFERENCES beer_recipe(id)
-    ON UPDATE CASCADE ON DELETE CASCADE # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE -- need to determine if this is correct...
 );
 
 CREATE TABLE grain_in_recipe(
@@ -96,10 +104,10 @@ CREATE TABLE grain_in_recipe(
   PRIMARY KEY (grain_id, recipe_id),
   FOREIGN KEY (recipe_id)
   REFERENCES beer_recipe(id)
-    ON UPDATE CASCADE ON DELETE CASCADE, # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE, -- need to determine if this is correct...
   FOREIGN KEY (grain_id)
   REFERENCES grain(id)
-    ON UPDATE CASCADE ON DELETE CASCADE # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE -- need to determine if this is correct...
 );
 
 CREATE TABLE hops_in_recipe(
@@ -110,22 +118,22 @@ CREATE TABLE hops_in_recipe(
   PRIMARY KEY (hops_id, recipe_id, exposure_time),
   FOREIGN KEY (recipe_id)
   REFERENCES beer_recipe(id)
-    ON UPDATE CASCADE ON DELETE CASCADE, # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE, -- need to determine if this is correct...
   FOREIGN KEY (hops_id)
   REFERENCES hops(id)
-    ON UPDATE CASCADE ON DELETE CASCADE # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE -- need to determine if this is correct...
 );
 
 CREATE TABLE yeast_in_recipe(
   yeast_id INT NOT NULL,
   recipe_id INT NOT NULL,
-  PRIMARY KEY (recipe_id), # because only 1 yeast per recipe
+  PRIMARY KEY (recipe_id), -- because only 1 yeast per recipe
   FOREIGN KEY (recipe_id)
   REFERENCES beer_recipe(id)
-    ON UPDATE CASCADE ON DELETE CASCADE, # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE, -- need to determine if this is correct...
   FOREIGN KEY (yeast_id)
   REFERENCES yeast(id)
-    ON UPDATE CASCADE ON DELETE CASCADE # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE -- need to determine if this is correct...
 );
 
 CREATE TABLE additive_in_recipe(
@@ -136,8 +144,8 @@ CREATE TABLE additive_in_recipe(
   PRIMARY KEY (additive_id, recipe_id, exposure_time),
   FOREIGN KEY (recipe_id)
   REFERENCES beer_recipe(id)
-    ON UPDATE CASCADE ON DELETE CASCADE, # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE, -- need to determine if this is correct...
   FOREIGN KEY (additive_id)
   REFERENCES additive(id)
-    ON UPDATE CASCADE ON DELETE CASCADE # need to determine if this is correct...
+    ON UPDATE CASCADE ON DELETE CASCADE -- need to determine if this is correct...
 );
--- Brewfection/data/populateBrewDB.sql
+++ Brewfection/data/populateBrewDB.sql
@@ -1,11 +1,11 @@
-# This should only be run once, right after running createBrewDB.
+-- This should only be run once, right after running createBrewDB.
 
-# Equipment Table
+-- Equipment Table
 
 INSERT INTO equipment(batch_size, extract_efficeny)
-VALUES(5,0.70); # default values, 5 gallon batch with 70% efficiency
+VALUES(5,0.70); -- default values, 5 gallon batch with 70% efficiency
 
-# Hops Table
+-- Hops Table
 INSERT INTO hops(name, description, alpha_acid, purpose)
 VALUES('Cascade 5.5AA', null, 5.5,'aroma'),
   ('Citra 11AA', null, 11,'dual'),
@@ -14,7 +14,7 @@ VALUES('Cascade 5.5AA', null, 5.5,'aroma'),
   ('Amarillo 9AA', null, 9,'dual'),
   ('Simcoe 12AA', null, 12,'bitter');
 
-# Grain Table
+-- Grain Table
 INSERT INTO grain(name, region, potential_extract, lovibonds)
 VALUES('Vienna Malt', null, 35, 4),
   ('White Wheat Malt', null, 37, 2),
@@ -42,7 +42,7 @@ VALUES('Vienna Malt', null, 35, 4),
   ('Munich Malt Light', null, 37, 6),
   ('Black Patent Malt', null, 26, 500);
 
-# Yeast Table
+-- Yeast Table
 INSERT INTO yeast(name, region, apparent_attenuation, yeast_type)
 VALUES('WLP001 California Ale', 'American', 0.75, 'ale'),
   ('WLP002 English Ale Yeast', 'English', 0.68, 'ale'),
@@ -72,13 +72,13 @@ VALUES('WLP001 California Ale', 'American', 0.75, 'ale'),
   ('WLP665 Flemish Ale Blend', 'Belgian', 0.71, 'ale'),
   ('WLP670 American Farmhouse Blend', 'American', 0.79, 'ale');
 
-# Additive Table
+-- Additive Table
 INSERT INTO additive(name, description, use_case)
 VALUES('Whirloc', null, 'fining'),
   ('Irish Moss', null, 'fining');
 
 
-# Beer Style Table
+-- Beer Style Table
 INSERT INTO beer_style(name, min_bitterness, max_bitterness, min_color, max_color, min_ABV, max_ABV)
 VALUES('American Imperial Porter', 35, 50, 39, 40, 7, 12),
   ('Hefeweizen', 10, 15, 3, 9, 4.9, 5.6),
@@ -91,134 +91,134 @@ VALUES('American Imperial Porter', 35, 50, 39, 40, 7, 12),
   ('American Imperial IPA', 65, 100, 5, 15, 7.5, 10.5),
   ('American Pale Ale', 20, 50, 3, 14, 4.2, 6.2);
 
-# Beer Recipes constructed below
+-- Beer Recipes constructed below
 
-# Build recipe for Hoppiness is an IPA American IPA
+-- Build recipe for Hoppiness is an IPA American IPA
 INSERT INTO beer_recipe(name, boil_time, description, instructions)
 VALUES('Hoppiness is an IPA', 60, 'For those in the “pursuit of hoppiness,” you won’t want to miss this one! With a nice variety of hops, this beer from Brewing Classic Styles by Jamil Zainasheff and John J. Palmer, is perfect for those who can’t get enough of hops.','https://www.homebrewersassociation.org/homebrew-recipe/beer-recipe-of-the-week-hoppiness-is-an-ipa/');
-# grabs last auto_increment id used
+-- grabs last auto_increment id used
 SET @recipe = (SELECT beer_recipe.id FROM beer_recipe WHERE beer_recipe.id=LAST_INSERT_ID());
-# Add 12.75lbs of 2-row pale malt
+-- Add 12.75lbs of 2-row pale malt
 SET @grain = (SELECT grain.id FROM grain WHERE name='2-row Pale Malt');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 12.75);
-# Add 2 lbs of munich malt
+-- Add 2 lbs of munich malt
 SET @grain = (SELECT grain.id FROM grain WHERE name='Munich Malt');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 2);
-# Add 1 oz of horizon hops
+-- Add 1 oz of horizon hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Horizon 13AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 60);
-# Add 1 oz of centennial hops
+-- Add 1 oz of centennial hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Centennial 9AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 10);
-# Add 1 oz of simcoe hops
+-- Add 1 oz of simcoe hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Simcoe 12AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 5);
-# Add 1 oz of amarillo hops
+-- Add 1 oz of amarillo hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Amarillo 9AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 0);
-# Add california ale yeast
+-- Add california ale yeast
 SET @yeast = (SELECT yeast.id FROM yeast WHERE name='WLP001 California Ale');
 INSERT INTO yeast_in_recipe(yeast_id, recipe_id)
 VALUES(@yeast,@recipe);
-# Add american ipa style
+-- Add american ipa style
 SET @style = (SELECT beer_style.id FROM beer_style WHERE name='American IPA');
 INSERT INTO style_of_recipe(style_id, recipe_id)
 VALUES(@style,@recipe);
 
-# Build recipe for Classic American Pale Ale
+-- Build recipe for Classic American Pale Ale
 INSERT INTO beer_recipe(name, boil_time)
 VALUES('Classic American Pale Ale', 60);
-# grabs last auto_increment id used
+-- grabs last auto_increment id used
 SET @recipe = (SELECT beer_recipe.id FROM beer_recipe WHERE beer_recipe.id=LAST_INSERT_ID());
-# Add 11lbs of 2-row pale malt
+-- Add 11lbs of 2-row pale malt
 SET @grain = (SELECT grain.id FROM grain WHERE name='2-row Pale Malt');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 11);
-# Add 0.5 lbs of crystal malt 40
+-- Add 0.5 lbs of crystal malt 40
 SET @grain = (SELECT grain.id FROM grain WHERE name='Crystal Malt 40');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 0.5);
-# Add 2 oz of amarillo hops
+-- Add 2 oz of amarillo hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Cascade 5.5AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 2, 60);
-# Add 2 oz of amarillo hops
+-- Add 2 oz of amarillo hops
 SET @hops = (SELECT hops.id FROM hops WHERE name='Cascade 5.5AA');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 2, 10);
-# Add california ale yeast
+-- Add california ale yeast
 SET @yeast = (SELECT yeast.id FROM yeast WHERE name='WLP001 California Ale');
 INSERT INTO yeast_in_recipe(yeast_id, recipe_id)
 VALUES(@yeast,@recipe);
-# Add american pale ale style
+-- Add american pale ale style
 SET @style = (SELECT beer_style.id FROM beer_style WHERE name='American Pale Ale');
 INSERT INTO style_of_recipe(style_id, recipe_id)
 VALUES(@style,@recipe);
 
-# Build recipe for Zombie Dust Clone
+-- Build recipe for Zombie Dust Clone
 INSERT INTO beer_recipe(name, boil_time, description, instructions)
 VALUES('Zombie Dust Clone Pale Ale', 60, null, 'https://www.brewersfriend.com/homebrew/recipe/view/280731/zombie-dust-clone-brain-eater');
-# grabs last auto_increment id used
+-- grabs last auto_increment id used
 SET @recipe = (SELECT beer_recipe.id FROM beer_recipe WHERE beer_recipe.id=LAST_INSERT_ID());
-# Add 11lbs of 2-row pale malt
+-- Add 11lbs of 2-row pale malt
 SET @grain = (SELECT grain.id FROM grain WHERE name='2-row Pale Malt');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 12);
-# Add 0.5 lbs of munich malt light
+-- Add 0.5 lbs of munich malt light
 SET @grain = (SELECT grain.id FROM grain WHERE name='Munich Malt Light');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 1.5);
-# Add 0.5 lbs of Melanoidin
+-- Add 0.5 lbs of Melanoidin
 SET @grain = (SELECT grain.id FROM grain WHERE name='Melanoidin');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 0.5);
-# Add 0.5 lbs of carapils
+-- Add 0.5 lbs of carapils
 SET @grain = (SELECT grain.id FROM grain WHERE name LIKE '%Carapils%');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 0.5);
-# Add 0.5 lbs of crystal malt 60
+-- Add 0.5 lbs of crystal malt 60
 SET @grain = (SELECT grain.id FROM grain WHERE name='Crystal Malt 60');
 INSERT INTO grain_in_recipe(grain_id, recipe_id, amount)
 VALUES(@grain,@recipe, 0.5);
-# Add 0.5 oz of citra hops
+-- Add 0.5 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 0.5, 60);
-# Add 1 oz of citra hops
+-- Add 1 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 15);
-# Add 1 oz of citra hops
+-- Add 1 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 10);
-# Add 1 oz of citra hops
+-- Add 1 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 5);
-# Add 1 oz of citra hops
+-- Add 1 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 1, 0);
-# Add 3 oz of citra hops
+-- Add 3 oz of citra hops
 SET @hops = (SELECT hops.id FROM hops WHERE name LIKE '%Citra%');
 INSERT INTO hops_in_recipe(hops_id, recipe_id, amount, exposure_time)
 VALUES(@hops,@recipe, 3, -7);
-# Add california ale yeast
+-- Add california ale yeast
 SET @yeast = (SELECT yeast.id FROM yeast WHERE name='WLP001 California Ale');
 INSERT INTO yeast_in_recipe(yeast_id, recipe_id)
 VALUES(@yeast,@recipe);
-# Add 3 oz of citra hops
+-- Add 3 oz of citra hops
 SET @additive = (SELECT additive.id FROM additive WHERE name LIKE '%Whirloc%');
 INSERT INTO additive_in_recipe(additive_id, recipe_id, amount, exposure_time)
 VALUES(@additive,@recipe, 1, 20);
-# Add american pale ale style
+-- Add american pale ale style
 SET @style = (SELECT beer_style.id FROM beer_style WHERE name='American Pale Ale');
 INSERT INTO style_of_recipe(style_id, recipe_id)
 VALUES(@style,@recipe);
