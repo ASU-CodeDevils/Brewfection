7b3eb404289caec89ea92fe4a76b4a9264ef6914
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-12-07 13:50:12 -0600

    Added pug for rendering the output of the hops test db query.

--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var path = require('path');
+var pug = require('pug');
 var serveStatic = require('serve-static');
 const { Pool, Client } = require('pg')
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
+app.set('views','./views');
+app.set('view engine', 'pug');
+app.engine('pug', pug.__express);
 
 
 app = express();
@@ -14,7 +18,7 @@ app.get('/db', function (request, response) {
         if (err)
             { console.error(err); response.send("Error " + err); }
         else
-            { response.render('pages/db', {results: res.rows} ); }
+            { response.render('hops', {results: res.rows} ); }
     });
 });
 var port = process.env.PORT || 5000;
