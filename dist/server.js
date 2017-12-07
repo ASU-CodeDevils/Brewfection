e3b068d27d184d254638e49e366ee8e5fbffb989
Kristofer Hoadley <kristoferhoadley@gmail.com>
2017-12-07 13:18:07 -0600

    Set up test connection to database.

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+
 app = express();
 app.use(serveStatic(__dirname));
+app.get('/db', function (request, response) {
+    pool.query('SELECT * FROM hops', function(err, res) {
+        if (err)
+            { console.error(err); response.send("Error " + err); }
+        else
+            { response.render('pages/db', {results: result.rows} ); }
+    });
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
